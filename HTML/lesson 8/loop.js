3bdd5954be3345a9413060068b4a00ce574aedc6
Afroz Muzammil <afroz.muzammil@autodesk.com>
2020-06-08 21:40:44 +0800

    print sum execercise

---
+++ HTML/lesson 8/loop.js
@@ -0,0 +1,28 @@
+function guessTheNumber()
+{
+
+    var inputNumber = Number(document.getElementById("number").value);
+    var result = "cannot guess it";
+    for (var i=1; i<=100; ++i) {
+
+        if (i == inputNumber) { // == equal to 20==20
+            result = i + "is the number";
+            break;
+        }
+
+
+    }
+    document.getElementById("result").innerText = result;
+}
+
+// 1+2+3+4+5+6+7+8+9+10=55
+// 1..100 = 5050
+
+function printSum() {
+    var inputNumber = Number(document.getElementById("number").value);
+    var result = "sum of 1 to " + inputNumber + " is";
+
+    /// calc sum of 1 to inputNumber
+
+    document.getElementById("result").innerText = result;
+}
