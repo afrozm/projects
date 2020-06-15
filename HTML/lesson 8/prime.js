52afb368f3fb571c4c8bd83b407c593ce81cd007
Afroz Muzammil <afroz.muzammil@autodesk.com>
2020-06-15 21:09:12 +0800

    prime

---
+++ HTML/lesson 8/prime.js
@@ -0,0 +1,16 @@
+function checkPrime() {
+    var number = Number(document.getElementById("number").value);
+    var isPrime = true;
+    for (var I=2; I<number; ++I) {
+        var remainder = number % I;
+        if (remainder == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    var result = "Number " + number + " is ";
+    if (!isPrime)
+        result += "not ";
+    result += "prime number";
+    document.getElementById("result").innerText = result;
+}
